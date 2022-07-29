a9de4fc5302de3f303f59992b800ff78d6366513
Praveen2812git <83016587+Praveen2812git@users.noreply.github.com>
2022-07-29 17:58:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('html1stpage.html')
+    return render_template('index.html')
 
 @app.route('/scrap', methods = ['POST'])
 def scrap_comments():
