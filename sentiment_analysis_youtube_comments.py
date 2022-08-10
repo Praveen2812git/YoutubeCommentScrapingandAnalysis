db99cb7ba27df0e95141584d8218708870c8a3d8
Praveen2812git <83016587+Praveen2812git@users.noreply.github.com>
2022-08-10 22:44:19 +0530

    Update sentiment_analysis_youtube_comments.py

--- sentiment_analysis_youtube_comments.py
+++ sentiment_analysis_youtube_comments.py
@@ -36,7 +36,23 @@ def sepposnegcom(comment_file):
 
     for (sentiment), group in dataset.groupby(['vader_sentiment']):
          group.to_csv(f'{sentiment}.csv', index=False)
- 
+    
+    if checkcsv.exists('1.csv') == False:                             # If 1.csv file does not exist, it creates one empty 1.csv file.
+        with open('1.csv', 'w', encoding='UTF8', newline='') as f1:
+            writer1 = csv.writer(f1)
+            header1 = ['Empty', 'Empty', 'Empty']
+            row1 = ['No Positive Comments', 'No Positive Comments', 'No Positive Comments']
+            writer1.writerow(header1)
+            writer1.writerow(row1)
+
+    if checkcsv.exists('0.csv') == False:                             # If 1.csv file does not exist, it creates one empty 1.csv file.
+        with open('0.csv', 'w',encoding='UTF8', newline='') as f0:
+            writer0 = csv.writer(f0)
+            header0 = ['Empty', 'Empty', 'Empty']
+            row0 = ['No Negative Comments', 'No Negative Comments', 'No Negative Comments']
+            writer0.writerow(header0)
+            writer0.writerow(row0)
+    
     pos = (pd.read_csv("1.csv", engine = 'python')).iloc[:, :-1]
     neg = (pd.read_csv("0.csv", engine = 'python')).iloc[:, :-1]
 
@@ -45,6 +61,11 @@ def sepposnegcom(comment_file):
 
     video_positive_comments = str(len(pos.axes[0])) + ' Comments'  #Finding total rows in positive comments
     video_negative_comments = str(len(neg.axes[0])) + ' Comments'  #Finding total rows in negative comments
+    
+    if (pd.read_csv('1.csv', nrows=0).columns.tolist())[0] == 'Empty':
+        video_positive_comments = '0 Comments'
+    if (pd.read_csv('0.csv', nrows=0).columns.tolist())[0] == 'Empty':
+        video_negative_comments = '0 Comments'
 
     ## return function
     return positive_comments, negative_comments, video_positive_comments, video_negative_comments
